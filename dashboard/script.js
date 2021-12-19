34900c70b68bf1b698473a848fc3b1ce5d6c101b
Jack Do <dominhtrijack@gmail.com>
2021-12-19 19:26:17 +0700

    child dashboard

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,68 +1,135 @@
 const apiUrl = ""
-let timeStamp = "hour"
+let timeStamp = "hour" // hour / day / month / year
 
 const updateList = async function () {
     // const usersInfo = await fetch(`${apiUrl}/users?by=${timeStamp}`).then(response => response.json())
     const usersInfo = [
         {
-            name: "Phạm Công",
-            url: "https://github.com/PhamCong01/weather-app",
-            publicRepos: ["repo1", "repo2"],
-            time: 12,
+            fullName: "Phạm Công",
+            username: "PhamCong01",
+            repos: [
+                {
+                    name: "Repo 1",
+                    description:
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tellus urna, porta non ligula id, pellentesque iaculis mauris. Aliquam quis nisl eget sem dapibus feugiat nec eget sapien. Nam pellentesque sem augue, in aliquet metus imperdiet a. Mauris sit amet malesuada justo, sed lobortis nunc",
+                    url: "https://github.com/PhamCong01/weather-app",
+                },
+                {
+                    name: "Repo 1",
+                    description:
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tellus urna, porta non ligula id, pellentesque iaculis mauris. Aliquam quis nisl eget sem dapibus feugiat nec eget sapien. Nam pellentesque sem augue, in aliquet metus imperdiet a. Mauris sit amet malesuada justo, sed lobortis nunc",
+                    url: "https://github.com/PhamCong01/weather-app",
+                },
+                {
+                    name: "Repo 1",
+                    description:
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tellus urna, porta non ligula id, pellentesque iaculis mauris. Aliquam quis nisl eget sem dapibus feugiat nec eget sapien. Nam pellentesque sem augue, in aliquet metus imperdiet a. Mauris sit amet malesuada justo, sed lobortis nunc",
+                    url: "https://github.com/PhamCong01/weather-app",
+                },
+                {
+                    name: "Repo 1",
+                    description:
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tellus urna, porta non ligula id, pellentesque iaculis mauris. Aliquam quis nisl eget sem dapibus feugiat nec eget sapien. Nam pellentesque sem augue, in aliquet metus imperdiet a. Mauris sit amet malesuada justo, sed lobortis nunc",
+                    url: "https://github.com/PhamCong01/weather-app",
+                },
+            ],
+            duration: 12,
         },
         {
-            name: "Jack Do",
-            url: "https://github.com/jackdo1012",
-            publicRepos: ["repo1", "repo2"],
-            time: 1,
+            fullName: "Phạm Công",
+            username: "PhamCong01",
+            repos: [
+                {
+                    name: "Repo 2",
+                    description: "Repo2 description is too long",
+                    url: "https://github.com/PhamCong01/weather-app",
+                },
+            ],
+            duration: 12,
         },
         {
-            name: "Jack Do",
-            url: "https://github.com/jackdo1012",
-            publicRepos: ["repo1", "repo2"],
-            time: 1,
+            fullName: "Phạm Công",
+            username: "PhamCong01",
+            repos: [
+                {
+                    name: "Repo 2",
+                    description: "Repo2 description is too long",
+                    url: "https://github.com/PhamCong01/weather-app",
+                },
+            ],
+            duration: 12,
         },
         {
-            name: "Test",
-            url: "https://github.com/test",
-            publicRepos: ["repo1", "repo2"],
-            time: 1145888,
+            fullName: "Phạm Công",
+            username: "PhamCong01",
+            repos: [
+                {
+                    name: "Repo 2",
+                    description: "Repo2 description is too long",
+                    url: "https://github.com/PhamCong01/weather-app",
+                },
+            ],
+            duration: 12,
         },
         {
-            name: "Ông Dev",
-            url: "https://github.com/ongdev",
-            publicRepos: ["repo1", "repo2"],
-            time: 1123,
+            fullName: "Phạm Công",
+            username: "PhamCong01",
+            repos: [
+                {
+                    name: "Repo 2",
+                    description: "Repo2 description is too long",
+                    url: "https://github.com/PhamCong01/weather-app",
+                },
+            ],
+            duration: 12,
         },
         {
-            name: "Phạm Công",
-            url: "https://github.com/PhamCong01/weather-app",
-            publicRepos: ["repo1", "repo2"],
-            time: 12,
+            fullName: "Phạm Công",
+            username: "PhamCong01",
+            repos: [
+                {
+                    name: "Repo 2",
+                    description: "Repo2 description is too long",
+                    url: "https://github.com/PhamCong01/weather-app",
+                },
+            ],
+            duration: 12,
         },
         {
-            name: "Jack Do",
-            url: "https://github.com/jackdo1012",
-            publicRepos: ["repo1", "repo2"],
-            time: 1,
+            fullName: "Phạm Công",
+            username: "PhamCong01",
+            repos: [
+                {
+                    name: "Repo 2",
+                    description: "Repo2 description is too long",
+                    url: "https://github.com/PhamCong01/weather-app",
+                },
+            ],
+            duration: 12,
         },
         {
-            name: "Jack Do",
-            url: "https://github.com/jackdo1012",
-            publicRepos: ["repo1", "repo2"],
-            time: 1,
+            fullName: "Phạm Công",
+            username: "PhamCong01",
+            repos: [
+                {
+                    name: "Repo 2",
+                    description: "Repo2 description is too long",
+                    url: "https://github.com/PhamCong01/weather-app",
+                },
+            ],
+            duration: 12,
         },
         {
-            name: "Test",
-            url: "https://github.com/test",
-            publicRepos: ["repo1", "repo2"],
-            time: 1145888,
-        },
-        {
-            name: "Ông Dev",
-            url: "https://github.com/ongdev",
-            publicRepos: ["repo1", "repo2"],
-            time: 1123,
+            fullName: "Phạm Công",
+            username: "PhamCong01",
+            repos: [
+                {
+                    name: "Repo 2",
+                    description: "Repo2 description is too long",
+                    url: "https://github.com/PhamCong01/weather-app",
+                },
+            ],
+            duration: 12,
         },
     ]
 
@@ -81,19 +148,17 @@ const updateList = async function () {
         // username
         const userNameNode = document.createElement("li")
         userNameNode.className = "item__name"
-        userNameNode.innerHTML = `<i class="far fa-smile"></i> Name: ${user.name}`
+        userNameNode.innerHTML = `<i class="far fa-smile"></i> Name: ${user.fullName}`
 
         // user url
         const urlNode = document.createElement("li")
         urlNode.className = "item__url"
-        urlNode.innerHTML = `<a class="link" target="_blank" href="${user.url}">GitHub: ${user.url}</a>`
+        urlNode.innerHTML = `<a class="link" target="_blank" href="https://github.com/${user.username}">GitHub: ${user.username}</a>`
 
         // url public repos
         const publicRepos = document.createElement("li")
         publicRepos.className = "repositories"
-        publicRepos.innerHTML = `Public repositories: <span>${user.publicRepos.length}</span>`
-
-        //
+        publicRepos.innerHTML = `Public repositories: <span>${user.repos.length}</span>`
 
         // append to userBasicInfo div
         userBasicInfoNode.appendChild(userNameNode)
@@ -103,20 +168,21 @@ const updateList = async function () {
         // time div
         const timeNode = document.createElement("div")
         timeNode.className = "time__item"
-        timeNode.innerHTML = `${user.time} hours`
+        timeNode.innerHTML = `${user.duration} hours`
 
         // create div for button
         const buttonContainer = document.createElement("div")
         buttonContainer.className = "button__container"
 
-        // create button see more
+        // create see more button
         const buttonMore = document.createElement("button")
         buttonMore.className = "buttonMore"
         buttonMore.innerHTML = "See more"
+        buttonMore.onclick = () => renderSeeMoreBtn(user.repos)
 
         // append button to div
         buttonContainer.appendChild(buttonMore)
-        
+
         // append userBasicInfo and time and buttonMore to item__container
         itemContainerNode.appendChild(userBasicInfoNode)
         itemContainerNode.appendChild(timeNode)
@@ -125,23 +191,96 @@ const updateList = async function () {
         // append item__container to main ul
         itemListNode.appendChild(itemContainerNode)
     })
+}
 
-    
-     
+const renderSeeMoreBtn = function (repos) {
+    const buttonContainer = document.querySelectorAll(".button__container")
+    const dashboardChildNode = document.querySelector(".dashboard__child")
+    const overLayNode = document.querySelector(".dashboard__overlay")
+    const exitIconNode = document.querySelector(".times-exit")
+
+    // child dashboard container
+    const dashboardContainerNode = document.querySelector(
+        ".dashboard__container"
+    )
+    dashboardContainerNode.innerHTML = ""
+
+    // click button open dashboard
+    buttonContainer.forEach((button) => {
+        button.onclick = () => {
+            dashboardChildNode.classList.add("active")
+            overLayNode.classList.add("active")
+        }
+    })
+
+    // click remove dashboard and overLay
+    overLayNode.onclick = () => {
+        dashboardChildNode.classList.remove("active")
+        overLayNode.classList.remove("active")
     }
-    
+    // click remove dashboard and overLay
+    exitIconNode.onclick = () => {
+        dashboardChildNode.classList.remove("active")
+        overLayNode.classList.remove("active")
+    }
+
+    repos.forEach((repo) => {
+        // each repos
+        const eachReposNode = document.createElement("div")
+        eachReposNode.className = "dashboard__child-content"
+
+        // repo basic info
+        const repoInfoNode = document.createElement("div")
+
+        // repo title
+        const repoTitleNode = document.createElement("h5")
+        repoTitleNode.innerHTML = repo.name
+        repoTitleNode.className = "content__title"
+
+        // repo description
+        const repoDescriptionNode = document.createElement("h5")
+        repoDescriptionNode.innerHTML = repo.description
+        repoDescriptionNode.className = "content__description"
+
+        // appen title and description to basic info
+        repoInfoNode.appendChild(repoTitleNode)
+        repoInfoNode.appendChild(repoDescriptionNode)
+
+        // go to div
+        const goToDivNode = document.createElement("div")
+        goToDivNode.className = "child__button"
+
+        // go to btn
+        const goToBtnNode = document.createElement("button")
+        goToBtnNode.innerHTML = "GO TO"
+        goToBtnNode.className = "button_go"
+        goToBtnNode.onclick = () => window.open(repo.url)
+
+        // append close btn to clost btn div node
+        goToDivNode.appendChild(goToBtnNode)
+
+        // append basic info and go to btn to each repos
+        eachReposNode.appendChild(repoInfoNode)
+        eachReposNode.appendChild(goToDivNode)
+
+        // append repo to dashboard
+        dashboardContainerNode.appendChild(eachReposNode)
+    })
+}
+
 window.onload = () => {
     updateList()
-    
+
     const timeStampHoursNode = document.querySelector(".time.time__hours")
     const timeStampDayNode = document.querySelector(".time.time__day")
     const timeStampMonthNode = document.querySelector(".time__month")
     const timeStampYearNode = document.querySelector(".time__year")
     const buttonContainer = document.querySelectorAll(".button__container")
     const dashboardChildNode = document.querySelector(".dashboard__child")
-    const overLay = document.querySelector(".dashboard__overlay")
-    const iconExit = document.querySelector(".times-exit")
+    const overLayNode = document.querySelector(".dashboard__overlay")
+    const exitIconNode = document.querySelector(".times-exit")
 
+    // change timestamp
     timeStampHoursNode.onclick = () => {
         timeStamp = "hour"
         timeStampHoursNode.classList.add("time__selected")
@@ -179,21 +318,21 @@ window.onload = () => {
     }
 
     // click button open dashboard
-    buttonContainer.forEach(button => {
+    buttonContainer.forEach((button) => {
         button.onclick = () => {
             dashboardChildNode.classList.add("active")
-            overLay.classList.add("active")
+            overLayNode.classList.add("active")
         }
     })
 
     // click remove dashboard and overLay
-    overLay.onclick = () => {
+    overLayNode.onclick = () => {
         dashboardChildNode.classList.remove("active")
-        overLay.classList.remove("active")
+        overLayNode.classList.remove("active")
     }
     // click remove dashboard and overLay
-    iconExit.onclick = () => {
+    exitIconNode.onclick = () => {
         dashboardChildNode.classList.remove("active")
-        overLay.classList.remove("active")
+        overLayNode.classList.remove("active")
     }
 }
