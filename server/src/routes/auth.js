4da2533e672311c907401b597dee0debee7833fc
yamngu <phuln8@gmail.com>
2021-12-22 19:36:56 +0700

    fix: require module

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken");
 const axios = require("axios");
 
 const User = require("../models/user");
